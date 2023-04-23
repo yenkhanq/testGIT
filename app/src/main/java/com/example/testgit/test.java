bfb0ad573584f2d868822ca3dc6e6b17e527ccbc
yen khang <Yenk946@gmail.com>
2023-04-23 19:47:08 +0700

    a commit

---
+++ app/src/main/java/com/example/testgit/test.java
@@ -0,0 +1,16 @@
+package com.example.testgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        //test commit a
+    }
+}
